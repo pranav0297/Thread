5ed3e7b2e4efbe60af76a85c62194fec2ea67402
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-09-27 01:13:20 +0530

    java_9.java

--- java_9.java
+++ java_9.java
@@ -1,4 +1,27 @@
-public class java_9 {
-}
+import java.util.concurrent.*;
+public class java_9 extends Thread {
+    private String clockName;
 
-//9---Increase concurrency with Thread pools using newCachedThreadPool() and newFixedThreadPool().
+    public java_9(String clockName) {
+        this.clockName = clockName;
+    }
+    public void run() {
+        for (int i = 5; i >= 0; i--) {
+            System.out.println(Thread.currentThread().getName()+"->"+ clockName+":"+ i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+          ExecutorService pool = Executors.newCachedThreadPool();                        //used for cached threadpool
+        //ExecutorService pool = Executors.newFixedThreadPool(2);                       //used for Fixed threadpool
+        pool.execute(new java_9("A"));
+        pool.execute(new java_9("B"));
+        pool.execute(new java_9("C"));
+        pool.execute(new java_9("D"));
+        pool.shutdown();
+    }
+}
