cbba5ca7b79eb18ec4f8699f86b106160828b8d6
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-09-27 01:42:10 +0530

    java_7.java

--- java_7.java
+++ java_7.java
@@ -1,3 +1,36 @@
-public class java_7 {
+import java.util.concurrent.*;
+import java.util.*;
+public class java_7{
+
+    public java_7(){
+        class MyCallable implements Callable<Integer>{
+            Integer id = 1;
+            public MyCallable(Integer val){
+                this.id = val;
+            }
+            public Integer call(){
+                return id;
+            }
+        }
+        System.out.println("creating List");
+        ExecutorService service = Executors.newFixedThreadPool(3);
+
+        List<MyCallable> futureList = new ArrayList<MyCallable>();
+        for (int i = 0; i < 10; i++){
+            MyCallable myCallable = new MyCallable((Integer)i);
+            futureList.add(myCallable);
+        }
+        System.out.println("Processing Start");
+        try{
+            List<Future<Integer>> futures = service.invokeAll(futureList);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("Processing Completed");
+        service.shutdown();
+    }
+
+    public static void main(String args[]){
+        java_7 demo = new java_7();
+    }
 }
-//7 ==Submit List of tasks to ExecutorService and wait for the completion of all the tasks.
